a6c3155485f17328476e743b04452cd4121f0897
Clare Robertson <clare.robertson@gaincredit.com>
2020-05-29 12:26:43 +0100

    Updates

---
+++ precache-manifest.d39f4d90de63c8ce05883e609d38ec92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a64c33357569151497a74ce85515f89",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "57a579b8bfff40b2620d",
+    "url": "/cocktails/static/css/main.25cb77be.chunk.css"
+  },
+  {
+    "revision": "0ec07ac535046e009f6c",
+    "url": "/cocktails/static/js/2.ffc3056a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails/static/js/2.ffc3056a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a579b8bfff40b2620d",
+    "url": "/cocktails/static/js/main.0d8ebe8a.chunk.js"
+  },
+  {
+    "revision": "150f1cf267e37ce89f61",
+    "url": "/cocktails/static/js/runtime-main.040d3ce3.js"
+  }
+]);
